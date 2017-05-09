9e487e7fc95eec30f1e7f37b90d6913711aa8657
Thomas von Dein <tom@izb.net>
2017-05-09 23:38:22 +0200

    +added code to determine remote home and do the chmod

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # tramp-secure-backups.el
 Emacs minor mode to securely store backups with fixed permissions
+
+# DRAFT
+This is a work in progress. Nothing usable here so far.
--- ssh-config.el
+++ ssh-config.el
@@ -38,12 +38,6 @@
    (line-beginning-position)
    (line-end-position)))
 
-(defun ssh-add-entry (sshlist host hostname user)
-  (when (and host (or user hostname))
-    (when (not user)
-      (setq user current-user))
-    (add-to-list 'sshlist (list (glob-to-regex host) user hostname) t)))
-
 (defun ssh-config-parse ()
   "Parse ~/.ssh/config and return content as alist."
   (interactive)
@@ -94,3 +88,66 @@ will be returned. Returned value is a cons cell. Access with car and cdr."
                       (throw 'break (cons U (or H N))))))))
 
 
+;;; run
+
+(setq sshlist (ssh-config-parse))
+
+(defun chomp (str)
+  "Chomp leading and tailing whitespace from STR."
+  (while (string-match "\\`\n+\\|^\\s-+\\|\\s-+$\\|\n+\\'" str)
+    (setq str (replace-match "" t t str)))
+      str)
+
+(defun get-remote-home (USER)
+  "Return home directory of USER. Works with tramp sessions too,
+because `shell-command` is being used which is `default-directory`
+agnostic. That is, if the current buffer (where the user hit
+C-x C-s) is a tramp buffer, then `shell-command`will be executed
+remotely via ssh."
+  (let ((home nil)
+        (shell "*Shell Command Output*"))
+      (with-current-buffer shell
+        (shell-command (format "ls -d ~%s" USER))
+        (setq home (chomp (buffer-substring-no-properties (point-min) (point-max)))))
+      (kill-buffer shell)
+      home))
+
+;; This advice will be executed  everytime emacs creates a backup.  it
+;; determines if  the file to  be backed up  is remote, finds  out the
+;; user it belongs to, and fixes the backup directory permissions.
+;; Finally it sets `backup-directory-alist appropriately.
+;;
+;; FIXME: test this crab!
+(advice-add
+ 'make-backup-file-name-1
+ :before
+ '(lambda (&rest file)
+    (let ((filename (car file))
+          (username nil)
+          (hostname nil)
+          (homedir nil)
+          (sshentry nil))
+      (cond
+       ((string-match "/ssh:\\(.+\\)@\\(.+\\):/" filename)
+        (setq username (match-string 1 filename))
+        (setq hostname (match-string 2 filename)))
+       ((string-match "/ssh:\\(.+\\):/" filename)
+        (setq hostname (match-string 1 filename))))
+      (if (not hostname)
+          ;; LOCAL backup, stick with default
+          (setq backup-directory-alist `(("." . ,my-backup-directory)))
+        ;; SSH backup, fix and customize
+        (setq sshentry (ssh-match sshlist hostname))
+        (if (not sshentry)
+            ;; not configured, use parsed results
+            (if (not username)
+                (setq username (user-login-name)))
+          ;; sshentry is filled
+          (if (not username)
+              ;; use user from config, else, use parsed one
+              (setq username (car sshentry))))
+        ;; now set up permissions and add the dir to backupdirs
+        (setq homedir (get-remote-home username))
+        ;; FIXME: when is the mkdir being executed? Maybe do it here before the chmod!
+        (shell-command (format "chmod 700 %s/.emacs.d/backups" homedir))
+        (setq backup-directory-alist `(("." . ,(format ":%s/.emacs.d/backups" homedir))))))))
